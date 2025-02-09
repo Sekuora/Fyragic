d17c6262d54cf02c37f1744f368dc462fec40dff
Sekuora <162371601+Sekuora@users.noreply.github.com>
2025-02-09 16:54:05 -0500

    CMake Build Setup
    Finally got a working cmake environment for the project.
    
    Co-Authored-By: Nino Ibarra <92058903+sylphorai@users.noreply.github.com>

--- EngineCore/Source/Fyragic/Log.cpp
+++ EngineCore/Source/Fyragic/Log.cpp
@@ -9,14 +9,14 @@ namespace Fengine
 	void Log::Init()
 	{
 		// Format Logger
-		spdlog::set_pattern("%^[%H:%M] %n: %v%$");
+		spdlog::set_pattern("%^[%H:%M:%S] %n: %v%$");
 
 		// Core Logger Data
-		CoreLogger = spdlog::stdout_color_mt("FyragicTraceLog");
+		CoreLogger = spdlog::stdout_color_mt("Log");
 		CoreLogger->set_level(spdlog::level::trace);
 
 		// Client Logger Data
-		ClientLogger = spdlog::stdout_color_mt("AppTraceLog");
+		ClientLogger = spdlog::stdout_color_mt("AppLog");
 		CoreLogger->set_level(spdlog::level::trace);
 	}
 
